e093f9e464593b0e06695695d385afad57ee4231
Adam Brightwell <abrightwell@lce.com>
2014-01-18 16:50:57 -0500

    prepared code base for initial programming.

--- src/org/first/team342/AerialAssistRobot.java
+++ src/org/first/team342/AerialAssistRobot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import org.first.team342.commands.CommandBase;
-import org.first.team342.commands.ExampleCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,23 +23,16 @@ import org.first.team342.commands.ExampleCommand;
  */
 public class AerialAssistRobot extends IterativeRobot {
 
-    Command autonomousCommand;
-
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
-
         // Initialize all subsystems
         CommandBase.init();
     }
 
     public void autonomousInit() {
-        // schedule the autonomous command (example)
-        autonomousCommand.start();
     }
 
     /**
@@ -51,11 +43,6 @@ public class AerialAssistRobot extends IterativeRobot {
     }
 
     public void teleopInit() {
-	// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        autonomousCommand.cancel();
     }
 
     /**
---
+++ src/org/first/team342/subsystems/Drive.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author abrightwell
+ */
+public class Drive extends Subsystem {
+
+    private static final Drive INSTANCE = new Drive();
+    
+    private Drive() {
+    }
+    
+    protected void initDefaultCommand() {
+    }
+    
+    public static Drive getInstance() {
+        return INSTANCE;
+    }
+}
