9128121bdda1f7c2f5eb301c765c968745908583
Adam Brightwell <abrightwell@lce.com>
2014-01-18 22:57:47 -0500

    added joystick to OI

--- src/org/first/team342/OI.java
+++ src/org/first/team342/OI.java
@@ -1,25 +1,26 @@
-
 package org.first.team342;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-     
-     private static final OI INSTANCE = new OI();
-     
-     private OI() {
-         Joystick stick = new Joystick(1);
-         Button button = new JoystickButton(stick, 1);
-     }
-}
 
+    private static final OI INSTANCE = new OI();
+
+    private Joystick driveJoystick = null;
+
+    private OI() {
+        this.driveJoystick = new Joystick(RobotMap.DRIVE_JOYSTICK_USB_PORT);
+    }
+
+    public static OI getInstance() {
+        return INSTANCE;
+    }
+
+    public Joystick getDriveJoystick() {
+        return this.driveJoystick;
+    }
+}
